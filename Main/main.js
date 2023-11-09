44ddeded96afe882fda8f0ce8fd1aede8ccb5d36
Aman <amanmittle1234@gmail.com>
2023-11-09 20:56:16 +0530

    Added main

---
+++ Main/main.js
@@ -0,0 +1,14 @@
+const slider = document.querySelector(".slider");
+ const slideWidth = slider.offsetWidth;
+ let currentSlide = 1;
+  function nextSlide() {
+      if (currentSlide < 4) {
+          currentSlide++;
+      } else {
+          currentSlide = 1;
+          slider.scrollBy({ left: (3 * slideWidth), behavior: "auto" });
+      }
+       slider.scrollBy({ left: slideWidth, behavior: "smooth" });
+  }
+ // Auto-slide every 5 seconds
+ setInterval(nextSlide, 4000);
